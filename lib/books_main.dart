d667084712159b9ca443da9ac917b1c544fe99f6
ömer kaplan <kaplan5019@gmail.com>
2024-08-09 11:38:18 +0200

    fetch datas from source and show

--- lib/books_main.dart
+++ lib/books_main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:path/path.dart' as p;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
@@ -14,63 +14,87 @@ class BooksMainpage extends StatefulWidget {
 }
 
 class _BooksMainpageState extends State<BooksMainpage> {
+  late Directory booksDirectory;
+  List allFilesFromDic = [];
+  @override
+  void initState() {
+    super.initState();
+    _listofFiles();
+  }
+
+  // Make New Function
+  void _listofFiles() async {
+    //get app directory
+    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+    booksDirectory = Directory('${appDocumentsDir.path}/books');
+    // create folder if not exists
+    if (!await booksDirectory.exists()) {
+      await booksDirectory.create(recursive: true);
+    }
+
+    setState(() {
+      allFilesFromDic = booksDirectory.listSync();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bücher"),
-        actions: [
-          //IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
-          PopupMenuButton<MenuItemCase>(
-              onSelected: (MenuItemCase selectedItem) async {
-                if (selectedItem == MenuItemCase.fromLocal) {
-                  //if selection is from Local, select file. then save it local folder of app.
-                  FilePickerResult? result = await FilePicker.platform
-                      .pickFiles(
-                          allowedExtensions: ["epub", "pdf"],
-                          type: FileType.custom);
+        appBar: AppBar(
+          title: const Text("Bücher"),
+          actions: [
+            PopupMenuButton<MenuItemCase>(
+                onSelected: (MenuItemCase selectedItem) async {
+                  if (selectedItem == MenuItemCase.fromLocal) {
+                    //if selection is from Local, select file. then save it local folder of app.
+                    await getFileFromLocal();
+                  } else {}
+                },
+                icon: const Icon(Icons.add),
+                itemBuilder: (context) => <PopupMenuEntry<MenuItemCase>>[
+                      const PopupMenuItem<MenuItemCase>(
+                        value: MenuItemCase.fromLocal,
+                        child: Text('Von lokal hinzufügen'),
+                      ),
+                      const PopupMenuItem<MenuItemCase>(
+                        value: MenuItemCase.fromUrl,
+                        child: Text('mit Url herunterladen'),
+                      ),
+                    ])
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: allFilesFromDic.length,
+          itemBuilder: (context, i) {
+            var myFile = allFilesFromDic[i] as File;
+            String extension = p.extension(myFile.path);
+            String fileName = p.basenameWithoutExtension(myFile.path);
 
-                  if (result != null) {
-                    File file = File(result.files.single.path!);
-                    String fileName = file.path.split('/').last;
+            return Column(
+              children: [
+                Text(fileName),
+                Text(extension),
+              ],
+            );
+          },
+        ));
+  }
 
-//get app directory
-                    final Directory appDocumentsDir =
-                        await getApplicationDocumentsDirectory();
-                    final newDirectory =
-                        Directory('${appDocumentsDir.path}/books');
+  Future<void> getFileFromLocal() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowedExtensions: ["epub", "pdf"], type: FileType.custom);
 
-                    // create folder if not exists
-                    if (!await newDirectory.exists()) {
-                      await newDirectory.create(recursive: true);
-                    }
+    if (result != null) {
+      File file = File(result.files.single.path!);
+      String fileName = file.path.split('/').last;
 
-                    // save file to the new folder
-                    final path = '${newDirectory.path}/$fileName';
-                    final localFile = await File(path).create();
-                    await localFile.writeAsBytes(await file.readAsBytes());
-                    print('Dosya yerel depolamaya kaydedildi: $path');
-                  } else {
-                    // User canceled the picker
-                  }
-                } else {}
-              },
-              icon: const Icon(Icons.add),
-              itemBuilder: (context) => <PopupMenuEntry<MenuItemCase>>[
-                    const PopupMenuItem<MenuItemCase>(
-                      value: MenuItemCase.fromLocal,
-                      child: Text('Von lokal hinzufügen'),
-                    ),
-                    const PopupMenuItem<MenuItemCase>(
-                      value: MenuItemCase.fromUrl,
-                      child: Text('mit Url herunterladen'),
-                    ),
-                  ])
-        ],
-      ),
-      body: const Column(
-        children: [],
-      ),
-    );
+      // save file to the new folder
+      final path = '${booksDirectory.path}/$fileName';
+      final localFile = await File(path).create();
+      await localFile.writeAsBytes(await file.readAsBytes());
+      print('Dosya yerel depolamaya kaydedildi: $path');
+    } else {
+      // User canceled the picker
+    }
   }
 }
